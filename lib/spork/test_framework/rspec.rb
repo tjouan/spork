0031bb85acaf0d1cc0a75b2b2afbf9488478ee39
Thibault Jouan <tj@a13.fr>
2014-06-23 20:12:57 +0000

    Support rspec 2.99.0 runner API

--- lib/spork/test_framework/rspec.rb
+++ lib/spork/test_framework/rspec.rb
@@ -7,12 +7,18 @@ class Spork::TestFramework::RSpec < Spork::TestFramework
       ::Spec::Runner::CommandLine.run(
         ::Spec::Runner::OptionParser.parse(argv, stderr, stdout)
       )
-    else
+    elsif use_rspec_command_line?
       ::RSpec::Core::CommandLine.new(argv).run(stderr, stdout)
+    else
+      ::RSpec::Core::Runner.run(argv, stderr, stdout)
     end
   end
 
   def rspec1?
     defined?(Spec) && !defined?(RSpec)
+  end
+
+  def use_rspec_command_line?
+    ::RSpec::Core::CommandLine.instance_methods(false).include? :run
   end
 end
